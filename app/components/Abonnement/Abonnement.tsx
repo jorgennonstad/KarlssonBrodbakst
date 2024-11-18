2ee488628dcf2d9b05eef31d1c2bd64ba578271c
Edgar Dahl <edgardahl123@gmail.com>
2024-11-18 16:26:03 +0100

    subscribe fix

--- app/components/Abonnement/Abonnement.tsx
+++ app/components/Abonnement/Abonnement.tsx
@@ -4,19 +4,19 @@ import React, { useState, useEffect } from 'react';
 import { client } from '@/app/lib/sanity';
 import './Abonnement.css';
 
-// Define the TypeScript interface for the Abonnement data
+// TypeScript interface for the Abonnement data
 interface AbonnementData {
   title: string;
   subtitle1: string;
   subtitle2: string;
   description: string;
-  backgroundImage: string;  // The background image URL
+  backgroundImage: string;  // Background image URL
   price: number;
   stripeProductId: string;
   stripePriceId: string;
 }
 
-// Sanity query to fetch subscription data
+// Fetch subscription data from Sanity
 async function getAbonnement(): Promise<AbonnementData> {
   const query = `
     *[_type == "abonnement"][0]{
@@ -57,18 +57,21 @@ export default function Abonnement() {
   if (error) return <div>{error}</div>;
 
   return (
-    <div className="abonnement-container" id='Abonnement-scroll'>
-      <div className="abonnement" style={{ backgroundImage: `url(${abonnement?.backgroundImage})` }}>
+    <div className="abonnement-container" id="Abonnement-scroll">
+      <div
+        className="abonnement"
+        style={{ backgroundImage: `url(${abonnement?.backgroundImage})` }}
+      >
         <div className="abonnement_overlay">
           <div className="abonnement-left">
             <h1>{abonnement.title}</h1>
             <h2>{abonnement.subtitle1}</h2>
             <h3>{abonnement.subtitle2}</h3>
-            <button className="subscribe-button">Abonner</button>
           </div>
           <div className="abonnement-right">
             <p>{abonnement.description}</p>
           </div>
+          <button className="subscribe-button">Abonner</button>
         </div>
       </div>
     </div>
