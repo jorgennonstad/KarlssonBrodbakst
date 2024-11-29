e931ea63f743b93335fc6dd77813abe446775d4d
jorgennonstad <jorgenon@stud.ntnu.no>
2024-11-30 00:55:56 +0100

    footer edit

--- app/components/Footer/footer.tsx
+++ app/components/Footer/footer.tsx
@@ -88,7 +88,7 @@ const Footer: React.FC = () => {
       </div>
 
       <div className="footer-bottom">
-        <p>&copy; 2024 Karlson Brødbakst. | <i>Designet og utviklet av Deviro.no</i></p>
+        <p>&copy; 2024 Karlson Brødbakst. | <i>Designet og utviklet av <span><a href="https://deviro.no/">Deviro.no</a></span></i></p>
       </div>
     </footer>
   );
