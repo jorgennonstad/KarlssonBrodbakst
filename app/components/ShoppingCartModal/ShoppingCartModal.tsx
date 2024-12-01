b9b015981a38c29e5bb333ddcc1d67b93bdd81ff
jorgennonstad <jorgenon@stud.ntnu.no>
2024-12-01 17:15:01 +0100

    oversatt til norsk

--- app/components/ShoppingCartModal/ShoppingCartModal.tsx
+++ app/components/ShoppingCartModal/ShoppingCartModal.tsx
@@ -53,14 +53,14 @@ export default function ShoppingCartModal() {
         <Sheet open={shouldDisplayCart} onOpenChange={handleCartClick}>
             <SheetContent className="sm:max-w-lg w-[90vw]">
                 <SheetHeader>
-                    <SheetTitle>Shopping Cart</SheetTitle>
+                    <SheetTitle>Handlevogn</SheetTitle>
                 </SheetHeader>
 
                 <div className="h-full flex flex-col justify-between z-100">
                     <div className="mt-8 flex-1 overflow-y-auto">
                         <ul className="-my-6 divide-y divide-gray-200">
                             {cartCount === 0 ? (
-                                <h1 className="py-6">You don't have any Items</h1>
+                                <h1 className="py-6">Du har ingen varer i handlekurven</h1>
                             ) : (
                                 <>
                                     {Object.values(cartDetails ?? {}).map((entry) => {
@@ -79,7 +79,7 @@ export default function ShoppingCartModal() {
                                                     <div>
                                                         <div className="flex justify-between text-base font-medium text-gray-900">
                                                             <h3>{entry.name}</h3>
-                                                            <p className="ml-4">${entry.price}</p>
+                                                            <p className="ml-4">Kr {entry.price}</p>
                                                         </div>
                                                         <p className="mt-1 text-sm text-gray-500 w-[90%] line-clamp-2">
                                                             {entry.description}
@@ -87,7 +87,7 @@ export default function ShoppingCartModal() {
                                                     </div>
 
                                                     <div className="flex flex-1 items-end justify-between text-sm">
-                                                        <p className="text-gray-500">QTY: {entry.quantity}</p>
+                                                        <p className="text-gray-500">Antall: {entry.quantity}</p>
 
                                                         <div className="flex">
                                                             <button
@@ -95,7 +95,7 @@ export default function ShoppingCartModal() {
                                                                 onClick={() => removeItem(entry.id)}
                                                                 className="font-medium text-primary hover:text-primary/80"
                                                             >
-                                                                Remove
+                                                                Fjern
                                                             </button>
                                                         </div>
                                                     </div>
@@ -110,21 +110,21 @@ export default function ShoppingCartModal() {
 
                     <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
                         <div className="flex justify-between text-base font-medium text-gray-900">
-                            <p>Subtotal:</p>
+                            <p>Tottal:</p>
                             <p>{totalPrice} Kr</p>
                         </div>
                         <p className="mt-0.5 text-sm text-gray-500">
-                            Shipping and taxes are calculated at checkout.
+                            Frakt velges og legges til ved checkout
                         </p>
                         <div className="m t-6">
                             <Button onClick={handleCheckoutClick} className="w-full">
-                                Checkout
+                                Gå til kassen
                             </Button>
                         </div>
                         <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                             <p>
-                                OR {""} 
-                                <button onClick={() => handleCartClick()} className="font-md text-primary hover:text-primary/80">Continue Shopping</button>
+                                ELLER {""} 
+                                <button onClick={() => handleCartClick()} className="font-md text-primary hover:text-primary/80">Fortsett å handle</button>
                             </p>
                         </div>
                     </div>
