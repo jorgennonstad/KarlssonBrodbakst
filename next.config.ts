32c553da0dfb290b106c1210630aedf4beda9c27
jorgennonstad <jorgenon@stud.ntnu.no>
2025-02-08 17:44:09 +0100

    build folder fixed

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  distDir: 'build', // Custom output directory
   images: {
     domains: ["cdn.sanity.io"],
-  }
+  },
 };
 
 export default nextConfig;
