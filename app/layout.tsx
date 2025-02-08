46f32622b2c6001f3d078f1174aab85dc08d9759
jorgennonstad <jorgenon@stud.ntnu.no>
2025-02-09 00:30:54 +0100

    maintenence mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import CartProvider from "./components/Providers";
 import Navbar from "./components/NavBar/Navbar";
 import ShoppingCartModal from "./components/ShoppingCartModal/ShoppingCartModal";
 
+// Add a boolean to toggle coming soon status
+const showComingSoon = true;  // Change this to `false` to show the normal content
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +20,20 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  if (showComingSoon) {
+    return (
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="bg-white pd-6 sm:pd-8 lg:pd-12 text-center">
+            <h1 className="text-4xl font-bold">Page Coming Soon</h1>
+            <p className="mt-4 text-lg">Karlson Brødbakst will be launching soon!</p>
+          </div>
+        </body>
+      </html>
+    );
+  }
+
+  // Otherwise, render the site normally
   return (
     <html lang="en">
       <body className={inter.className}>
