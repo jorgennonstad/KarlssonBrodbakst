607868993a186e41aeea9e41aa71b115e6ea5b32
edgardahl <72726911+edgardahl@users.noreply.github.com>
2025-02-10 01:24:59 +0100

    style fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,11 @@ export default function RootLayout({
     return (
       <html lang="en">
         <body className={inter.className}>
-          <div className="bg-white pd-6 sm:pd-8 lg:pd-12 text-center">
-            <h1 className="text-4xl font-bold">Nettsiden kommer snart</h1>
-            <p className="mt-4 text-lg">Karlson Brødbakst lanseres snart!</p>
-          </div>
+        <div className="bg-black text-white text-center mt-8 p-6 sm:p-8 lg:p-12">
+          <h1 className="text-4xl font-bold">Nettsiden kommer snart</h1>
+          <p className="mt-4 text-lg">Karlson Brødbakst lanseres snart!</p>
+        </div>
+
         </body>
       </html>
     );
