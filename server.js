7979760c519ab5d715c829b320cfb7d4a6871469
jorgennonstad <jorgenon@stud.ntnu.no>
2025-02-11 17:38:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://karlsonbrodbakst.netlify.app", // Default to production if CLIENT_URL is not defined
+    origin: process.env.CLIENT_URL || "https://karlssonbrodbakst.netlify.app/", // Default to production if CLIENT_URL is not defined
   })
 );
 
